c567260c37a11fb0abda836285f7a37549238e53
Rony Cesana <rony.cesana@thoughtworks.com>
2021-12-20 16:33:21 +0100

    Initial import

---
+++ src/main/java/org/openapitools/codegen/maximumlimit/MaximumLimitApplication.java
@@ -0,0 +1,13 @@
+package org.openapitools.codegen.maximumlimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximumLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaximumLimitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/openapitools/codegen/maximumlimit/MaximumLimitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openapitools.codegen.maximumlimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaximumLimitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
